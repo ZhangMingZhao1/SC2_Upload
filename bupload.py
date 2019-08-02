93639f1c4bad03cd8707e418216ece4beb2e2e8f
ZhangMingZhao1 <1104272319@qq.com>
2019-08-03 03:05:59 +0800

    1

--- bupload.py
+++ bupload.py
@@ -8,12 +8,20 @@ b = Bilibili()
 login_status = b.login('15073124380', 'zhang1314nmopM')
 print("Login:", login_status)
 
-videoPart1 = [VideoPart("Let's see how this robot boogies - AlphaStar Breakdown Pt. 1-8AfzMA3Cvio.mkv", '已发现的欧服天梯阿尔法星人族replay.mkv'),VideoPart("Let's see how this robot boogies - AlphaStar Breakdown Pt. 2-i1qBpgaSWHU.mkv", '已发现的欧服天梯阿尔法星虫族replay.mkv')]
+videoPart1 = VideoPart("爆笑星际虚空之遗-aDE9VCYAnc8.mp4", '爆笑星际虚空之遗-aDE9VCYAnc8.mp4')
+videoPart2 = VideoPart("TheBeast vs AlphaStar [ZvZ] Deepmind A.I. Starcraft 2-ckbVqwhkY60.mkv", '已发现的欧服天梯阿尔法星replay(三)ZVZ')
+videoPart3 = VideoPart("Alphastar vs Stekepanne [TvZ] Deepmind A.I. Starcraft 2-hVy47vr5WaI.mkv", '已发现的欧服天梯阿尔法星replay(四)TVZ')
+
 
 
 tags = {u"星际争霸2", u"AlphaStar", u"人工智能", u"机器人", u"暴雪游戏"}
+tags = {u"星际争霸2", u"动画", u"搞笑", u"暴雪游戏",u"游戏CG"}
 
-b.upload(parts=videoPart1, title='已发现的欧服天梯阿尔法星replay(二)', tid=65, tag=tags, desc='已发现的欧服天梯阿尔法星replay人族和虫族',cover=b.cover_up("cover.jpg"),open_elec=1)
+b.upload(parts=videoPart1, title='爆笑星际mod版的虚空之遗CG宣传片', tid=65, tag=tags, desc='爆笑星际mod版的虚空之遗CG宣传片',source="https://www.youtube.com/watch?v=aDE9VCYAnc8",open_elec=1)
+
+b.upload(parts=videoPart2, title='已发现的欧服天梯阿尔法星replay(三)ZVZ', tid=65, tag=tags, desc='已发现的欧服天梯阿尔法星replay(三)ZVZ',source="https://www.youtube.com/watch?v=ckbVqwhkY60",cover=b.cover_up("cover.jpg"),open_elec=1)
+
+b.upload(parts=videoPart3, title='已发现的欧服天梯阿尔法星replay(四)TVZ', tid=65, tag=tags, desc='已发现的欧服天梯阿尔法星replay(四)TVZ',source="https://www.youtube.com/watch?v=hVy47vr5WaI",cover=b.cover_up("cover.jpg"),open_elec=1)
 
 
 
